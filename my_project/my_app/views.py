e0eea448808228391415afd37d06fa66d51fd13f
M1KEEEE <72807912+M1KEEEE@users.noreply.github.com>
2022-10-08 14:33:57 +0300

    Add files via upload

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+	now = datetime.now()
 	return render(
 		request, 
 		"my_app/index.html", 
-		{"new_year": ""})
+		{"is_new_year": now.day == 1 and now.month == 1})
